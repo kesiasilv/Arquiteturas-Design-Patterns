e4a295dd81e72bfa18c9abe51eb2bfe83f35cc78
Késia Silva <kesiasilvab6@gmail.com>
2025-11-04 18:00:40 -0300

    projeto de estudo feito

--- StudyObservable/StudyObservable/ContentView.swift
+++ StudyObservable/StudyObservable/ContentView.swift
@@ -6,14 +6,33 @@
 //
 
 import SwiftUI
+//import Combine //depois ver porque é necessario importar isso pro @publish funcionar (quando fiz da mandeira antiga)
+import Observation
+
+@Observable
+class ViewModel {
+    init() {}
+    
+    var count = 99942
+    
+}
 
 struct ContentView: View {
+    var viewModel = ViewModel()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("IOS Academy!!")
+                .padding()
+            Text("\(viewModel.count)")
+                .font(.system(size: 32, weight: .semibold))
+            Text("Subscribers")
+                .font(.system(size: 32, weight: .semibold))
+            
+            Button("Add subscriber"){
+                viewModel.count += 1
+            }
+            .padding()
         }
         .padding()
     }
